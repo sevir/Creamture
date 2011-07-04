bff1034f750873bd8166e1cf958520a9597b3081
Jose Francisco Rives <jose@sevir.org>
2011-07-04 14:07:00 +0200

    remove the test module

--- application/modules/prueba/helpers/migato_helper.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-function smple(){
-	echo 'Hello World';
-}
-
-/* eof application/modules/prueba/helpers/migato_helper.php */
