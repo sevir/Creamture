8e8a4b7d15bc1d98ab4896da8f0b802699e1cfb1
Jose Francisco Rives <jose@sevir.org>
2012-10-09 17:29:25 +0200

    Missed language conf

---
+++ application/config/language.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['domain'] = 'creamture';
+$config['default_locale'] = 'es_ES';
+$config['available_locales'] = array('en_UK');
+$config['locale_dir'] = APPPATH.'../i18n/locales/';
+$config['encoding'] = 'UTF-8';
+$config['twig_tmp_locale_dir'] = APPPATH.'../i18n/twig_tmp/';
+
+/* eof */
