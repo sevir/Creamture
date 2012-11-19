06de279fcc2a9b7acda855c99b39b0393420bf6d
Jose Francisco Rives <jose@sevir.org>
2012-11-19 12:38:22 +0100

    Rest example fixed

--- application/modules/rest_api/controllers/key.php
+++ application/modules/rest_api/controllers/key.php
@@ -13,7 +13,7 @@
 */
 
 // This can be removed if you use __autoload() in config.php
-require(APPPATH.'/modules/api/libraries/REST_Controller.php');
+require(APPPATH.'/modules/rest_api/libraries/REST_Controller.php');
 
 class Key extends REST_Controller
 {
